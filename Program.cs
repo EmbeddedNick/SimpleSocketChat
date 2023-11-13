7bf7dd98a7754e1a49f8149054d86fa58da38119
Nick <quickanddirtymail@yandex.ru>
2023-11-13 21:22:55 +0700

    Support Client-Server chatting protocol

--- Program.cs
+++ Program.cs
@@ -11,22 +11,97 @@ namespace ConsoleServer
 {
     internal class Program
     {
+        static Dictionary<string, Socket> _clients = new Dictionary<string, Socket>();
         const int SERVER_PORT = 35072;
+        enum EClientState 
+        {
+            WaitingForAutorization,
+            Chatting
+        }
+        enum EChatCode 
+        {
+            Autorization = 192,
+            Chatting = 191,
+            EmptyNickName = 3,
+            AutorizationFinishedGood = 0,
+            MessageAdded = 0,
+            StringAlreadyConsist = 1,
+            WrongCommandCode = 4
+        }
         static void ClientThread(object obj) 
         {
             Socket client = obj as Socket;
+            EClientState state = EClientState.WaitingForAutorization;
             byte[] buf = new byte[1024];
             int receivedMessageLength;
+            string nickName="";
+            string message;
             try
             {
                 while (client.Connected)
                 {
                     receivedMessageLength = client.Receive(buf, 0, 1024, SocketFlags.None);
-                    Console.WriteLine("Received message \"{0}\"", Encoding.UTF8.GetString(buf, 0, receivedMessageLength));
+                    if (receivedMessageLength > 0)
+                    {
+                        switch (state)
+                        {
+                            case EClientState.WaitingForAutorization:
+                                switch ((EChatCode)buf[0])
+                                {
+                                    case EChatCode.Autorization:
+                                         nickName = Encoding.UTF8.GetString(buf, 1, receivedMessageLength - 1);
+                                        if (string.IsNullOrWhiteSpace(nickName))
+                                        {
+                                            client.Send(new byte[1] { (byte)EChatCode.EmptyNickName });
+                                        }
+                                        else if (_clients.Keys.Contains(nickName))
+                                        {
+                                            client.Send(new byte[1] { (byte)EChatCode.StringAlreadyConsist });
+                                        }
+                                        else
+                                        {
+                                            client.Send(new byte[1] { (byte)EChatCode.AutorizationFinishedGood });
+                                            _clients.Add(nickName, client);
+                                            state = EClientState.Chatting;
+                                        }
+                                        break;
+                                    case EChatCode.Chatting:
+                                    default:
+                                        client.Send(new byte[1] { (byte)EChatCode.WrongCommandCode });
+                                        break;
+
+                                }
+                                break;
+                            case EClientState.Chatting:
+                                switch ((EChatCode)buf[0]) 
+                                {
+                                    case EChatCode.Chatting:
+                                    
+                                        message =
+                                            Encoding.UTF8.GetString(buf, 1, receivedMessageLength - 1);
+                                        Console.WriteLine("{0}:{1}", nickName, message);
+                                        client.Send(new byte[1] { (byte)EChatCode.MessageAdded });
+                                        break;
+                                    default:
+                                        client.Send(new byte[1] { (byte)EChatCode.WrongCommandCode });
+                                        break;
+                                }
+                                break;
+                            default:
+                                client.Send(new byte[1] { (byte)EChatCode.WrongCommandCode });
+                                break;
+                        }
+
+                    }
+                
                 }
             }
             catch (Exception ex)
             {
+                if (_clients.Keys.Contains(nickName)) 
+                {
+                    _clients.Remove(nickName);
+                }
                 client.Shutdown(SocketShutdown.Both);
                 client.Close();
             }
